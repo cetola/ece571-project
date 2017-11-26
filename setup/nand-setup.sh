03be73b1a01ea22d1ed1d89fa9d4c66f4a358eed
Stephano Cetola <stephanoc@gmail.com>
2017-11-25 16:09:53 -0800

    nand-setup.sh: add brief explanation
    Signed-off-by: Stephano Cetola <stephanoc@gmail.com>

--- setup/nand-setup.sh
+++ setup/nand-setup.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
+
+##Setup script for testing NAND in ModelSim.
+##Run this script from the root project directory.
+
+#Create test dir and move files there
 mkdir nand-con
 cp hdl/nand/* nand-con/
 cp hvl/nand/* nand-con/
 cp setup/* nand-con/
+
+#CD into nand-con and setup git for change tracking.
 cd nand-con
 git init
 git add --all
