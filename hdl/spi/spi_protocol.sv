5350d305842dffabcba23f54f04780578a059f9b
Stephano Cetola <stephano.cetola@linux.intel.com>
2017-12-08 17:38:06 -0800

    SPI updates
    Signed-off-by: Stephano Cetola <stephano.cetola@linux.intel.com>

--- hdl/spi/spi_protocol.sv
+++ hdl/spi/spi_protocol.sv
@@ -1,12 +1,14 @@
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//// 
-////
-////
-////
-interface SPIBus();
+////Created By: kaustubh Agashe. Started off with the in class code on SPI and modified it.//////////////////////////////////////
+////Description: Basic functionality of master and slave in master module and slave module respectively./////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-parameter NUMSLAVES = 1;
-parameter type DTYPE = logic [7:0];
+interface SPIBus(); 				//SPI INTERFACE
+
+parameter NUMSLAVES = 1;			//parameterising number of slaves
+parameter type DTYPE = logic [7:0];	
+
+import usefull::*;
 
 logic SCLK;
 logic MOSI;
@@ -18,6 +20,7 @@ output MISO,
 input SCLK,
 input MOSI,
 input SS
+
 );
 
 modport Master(
@@ -25,68 +28,107 @@ input MISO,
 output SCLK,
 output MOSI,
 output SS
+
 );
-
-
-
 endinterface
 
-module SPISlave #(parameter type DTYPE = logic [7:0], parameter ID = 0) (SPIBus SPI);
+//////////////////////////////////SLAVE MODULE////////////////////////////////////////////////
+module SPISlave #(parameter type DTYPE = logic [7:0], parameter ID = 0) (SPIBus.Slave SPI);
 
 DTYPE SR;
 initial
 $display("slave called");
+
 assign SPI.MISO[ID] = SPI.SS[ID] ? 'z : SR[0];
 
-always_ff @(negedge SPI.SCLK)
-if (SPI.SS[ID] === 0)
+always_ff @(negedge SPI.SCLK) begin
+
+if (SPI.SS[ID] === 0) begin
+
 SR <= {SPI.MOSI, SR[$bits(DTYPE)-1:1]};
+end
+//$display($time," clk=%b, id=%b,SR=%b",SPI.SCLK,ID,SR);
+end
 
-
-always@(SR)
-$display("id=%b,sr=%b",ID,SR);
 endmodule
 
-module SPIMaster #(parameter type DTYPE = logic [7:0]) (SPIBus SPI, input DTYPE Data,
-input int ID, input logic Request, output logic Done);
+///////////////////////////MASTER MODULE////////////////////////////////////////////////////////////
 
+module SPIMaster #(parameter type DTYPE = logic [7:0], parameter clkwidth = 100) (SPIBus.Master SPI, input DTYPE Data,
+				   input int ID, input logic Request, output logic Done, input mainclk, input reset);
+ 
 DTYPE MR;
+
+
+enum {OFF,ON} load_MR,tristate;
 
 assign SPI.MOSI = MR[0];
 
+
+
 initial
+
+
 $display("master called");
+
 
 always_ff @(negedge SPI.SCLK)
 begin
-if($rose(Request))
-MR <= Data;
-else 
-MR <= { &SPI.MISO, MR[$bits(DTYPE)-1:1]}; // delay for setup time of slave?
+$display("inside masters always negedge block. tristate=%d, load_MR=%d",tristate,load_MR);
+
+if(tristate==ON) begin
+MR <= 'z;
 end
+else if(load_MR==ON) begin
+	MR <= Data;
+end
+else begin 
+MR <= { SPI.MISO, MR[$bits(DTYPE)-1:1]}; 
+
+end
+end
+
+always_ff@(posedge mainclk, negedge mainclk)begin
+if(reset)
+tristate = ON;
+else if($rose(Request))begin
+load_MR = ON;
+tristate = OFF;
+end
+else begin
+load_MR = OFF;
+tristate = OFF;
+end
+
+end
+
+
 
 always @(Request)
 begin
 $display("in SPI Master always block. $bits(DTYPE) = %d",$bits(DTYPE));
-begin
+
 $display("in SPIMaster after checking Request/Done");
 Done = 0;
 SPI.SS[ID] = 0;
-//MR = Data;
+
 #100;
 if(Request) begin
 for (int i = 0; i <= $bits(DTYPE); i++)
 begin
 $display("In SPIMaster loop, i = %d",i);
+
 SPI.SCLK = 1;
-#100
+#(clkwidth)
 SPI.SCLK = 0;
-#100;
+#(clkwidth);
 end
 $display("...out of loop");
-Done = 1;
-SPI.SS[ID] = 1;
+Done = 1'b1;
+
+SPI.SS[ID] = 1'b1;
 end
 end
-end
+
 endmodule
+
--- hdl/spi/spi_protocoltb.sv
+++ hdl/spi/spi_protocoltb.sv
@@ -1,74 +1,174 @@
-module top;
-parameter data_width = 32;
-parameter NUMSLAVES = 12;
-parameter SS = 10;
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/////Created By : Kaustubh Agashe
+/////Description : top module ----> module upTop : instantiates module top which models the testbench functionality.
+/////number of slaves, slave select, and data width of the data to be transferred can be changed through the 
+////module upTop. 
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+package Randomize;            		//class created. used for randomisation of NUMSLAVES,SS, data_width.
+class Packet;
+int data_width = 8;
+randc bit [7:0] D;
+rand int SS,NUMSLAVES;
+static int count = 0; 			// Number of objects created
+int id;
+
+constraint c {
+		D inside {[1:data_width]};
+	     }
+
+constraint d {SS < 10;}
+
+constraint e {NUMSLAVES < 32 ;}
+
+endclass
+
+endpackage
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+module SPItb #(
+	parameter data_width = 8,
+	parameter NUMSLAVES = 12,
+	parameter clkwidth = 100,
+	parameter SS = 10);
+	
+	
+
 typedef logic [data_width-1:0] DTYPE;
+
+
+import Randomize::*;
+
+logic mainclk,reset;		//clock added
+
 DTYPE D;
-DTYPE temp;
-int S;
+
+DTYPE temp,j;			//to record the original data to be transferred.
+
+
+
+int S,passed,failed,total_tests;
+int index;
 bit Request;
 bit Done;
+
 genvar i;
-SPIBus #(.DTYPE(DTYPE),.NUMSLAVES(NUMSLAVES)) SPI();
-generate 
+
+SPIBus #(.DTYPE(DTYPE),.NUMSLAVES(NUMSLAVES)) SPI(); //SPI interface instantiation.
+
+generate 					//SPI slave connections generated depending on NUMSLAVES.
 for(i = 1'b0; i < NUMSLAVES; i=i+1'b1)
 begin : loop
 SPISlave #(.DTYPE(DTYPE),.ID(i)) Slave(SPI);
-end
+end : loop
 endgenerate
-SPIMaster #(.DTYPE(DTYPE)) M1(SPI,D,S,Request,Done);
 
-always @ (D)
+SPIMaster #(.DTYPE(DTYPE),.clkwidth(clkwidth)) M1(SPI,D,S,Request,Done,mainclk,reset);
+							//SPI master instantiated
+always @ (D)begin		//update data
 temp = D;
-
-
-initial
-begin
-$display("In initial block");
-S = 10;
-D = 16'b1001001110010011;
-Request = 0;
-#100 Request = 1;
-
-
-wait(Done);
-$display("MR = %b, Done=%b",M1.MR,Done);
-if((top.loop[SS].Slave.SR === temp))
-$display("TEST PASSED");
-else
-$display("TEST FAILED");
-//$display("SR[0] = %b",S0.SR);
-//$display("SR[1] = %b",S1.SR);
-
-#100 Request = 0;
-#100 Request = 1;
-#100 S = 10;
-D = 8'b10010011;
-
-
-
-wait(Done);
-$display("MR = %b, Done=%b",M1.MR,Done);
-if((top.loop[SS].Slave.SR === temp))
-$display("TEST PASSED");
-else
-$display("TEST FAILED");
-//$display("SR[0] = %b",S0.SR);
-//$display("SR[1] = %b",S1.SR);
-
-
-
-
-$stop();
+$display("D=%d",temp);
 end
 
-always@(M1.MR,Done,Request)
-$display($time,"Request=%b, MR=%b, Done=%b",Request,M1.MR,Done);
+always #clkwidth mainclk=~mainclk;
 
-//final
-//$display("MR=%b,S0.SR=%b,S1.SR=%b,done=%b",M1.MR,S0.SR,S1.SR,Done);
+initial begin
+Packet p;			//handle created
+p = new();			//object created
+			
+p.data_width = data_width;	//update randomized data width
+reset = 1'b0;			
+mainclk=1'b0;
+$display("In initial block. packetCount=%d",p.count);
+
+passed=0;
+failed=0;
+total_tests=0;
+
+for(int j=0; j<10; j=j+1) begin	 	//generating testcases with randomoized data width,NUMSLAVES and SS.
+$display("INSIDE J LOOP");
+#200 Request = 0;
+#100 Request = 1;
+     S = SS;
+assert (p.randomize(D)) $display("randomization successful");		//randomizing data
+else $fatal(0, "Packet::randomize failed");
+	
+     D = p.D;
+
+/////Self testing results
+wait(Done);				
+$display("clk=%b, MR = %b, SR %d = %b, Done=%b",SPI.SCLK,D,SS,SPItb.loop[SS].Slave.SR,Done);
+if(((SPItb.loop[SS].Slave.SR) === temp)) begin
+$display("TEST PASSED");
+passed=passed+1;
+end
+else begin
+$display("TEST FAILED");
+failed=failed+1;
+end   
+end
+
+for(int z=0;z<6;z=z+1) begin		//checking for all 0s, all 1s, Xs and Zs
+$display("INSIDE Z LOOP");
+
+#200 Request = 0;
+#100 Request = 1;
+     S = SS;
+if(z==0)  D = '0;	
+else if(z==1)  D = '1;
+else if(z==2)  D = 'x;	
+else if(z==3)  D = {(data_width/2){2'b10}};	
+else if(z==4)  D = {(data_width/2){2'b01}};	
+else D = 'z;
+	
+
+//Self testing
+wait(Done);
+$display("clk=%b, MR = %b, SR %d = %b, Done=%b",SPI.SCLK,D,SS,SPItb.loop[SS].Slave.SR,Done);
+if(((SPItb.loop[SS].Slave.SR) === temp))begin
+$display("TEST PASSED");
+passed=passed+1;
+end
+else begin
+$display("TEST FAILED");
+failed=failed+1;
+end
+end
+ $stop;
+end
+
+
+
+always@(M1.MR,Done,Request,SPI.SCLK,mainclk,SPItb.loop[SS].Slave.SR,reset)			
+$display($time," mainclk=%b, reset=%b, spi.sclk=%b, Request=%b, MR=%b, Done=%b, SR=%b",mainclk,reset,SPI.SCLK,Request,M1.MR,Done, SPItb.loop[SS].Slave.SR);
+
+final begin					//Check and print test results.
+$display("NUMSLAVES=%d, datawidth=%d, SS=%d",NUMSLAVES,data_width,SS);
+$display("total tests=%d, passed=%d, failed=%d",passed+failed,passed,failed);
+if(failed==0) $display("TEST PASSED ! ! !");
+else $display("TEST FAILED ! ! !");
+end
+endmodule
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////TOP///////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////MODULE/////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+module upTop;
+
+
+
+import Randomize::*;
+
+
+initial begin
+Packet packet;
+packet=new();
+
+end
+//works for data_width > 1 with no upper limit and variable number of slaves with NUMSLAVES > 0 and slave select <= NUMSLAVES.
+SPItb #(.data_width(16),.NUMSLAVES(10),.SS(2)) SPI_protocol();
 
 endmodule
-//////////////////////////////////////////////////////////////////////////////////////
-
-
